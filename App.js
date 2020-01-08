0e3eb0c15ab64edd29b7ba2633dd6a3f1fc06108
Justin Jr <justin.jr@cyberskill.tech>
2020-01-08 14:03:46 +0700

    Initialize

---
+++ App.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import Homescreen from "./screens/Home";
+
+export default function App() {
+  return <Homescreen />;
+}
---
+++ screens/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default props => {
+  return (
+    <View>
+      <Text>This is my new home</Text>
+    </View>
+  );
+};
